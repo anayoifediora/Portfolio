6a82cf3208043bbba47c57b5e965eb6b1fbd812c
Kanayo <anayoifediora@live.com>
2023-08-06 14:17:42 +0930

    worked on Portfolio styling

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,15 +1,49 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Weather from '../../Assets/Weather Dashboard.png';
+import Employee from '../../Assets/Employee database.png';
+import Fishspots from '../../Assets/10-marlin.gif';
+import Scheduler from '../../Assets/Workday Scheduler.png';
+
 
 const Portfolio = () => {
+
     return (
-        <div>
+        <div className='projects-container'>
             <h2 className='page-titles'>Portfolio</h2>
-            <div class="card" style={{width: '18rem'}}>
-                <img src="..." class="card-img-top" alt="..."></img>
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <a href="#" class="card-link">Github</a>
-                    <a href="#" class="card-link">Deployed</a>
+            <div className='row' style={{display: 'flex', justifyContent: 'center'}}>
+                <div className="card" style={{width: '35rem'}}>
+                    <img src={Weather} className="app-image card-img-top" alt="..."/>
+                    <div class="card-body">
+                        <h5 class="card-title">Weather ForeCast Dashboard</h5>
+                        <a href="#" class="card-link">Github</a>
+                        <a href="#" class="card-link">App link</a>
+                    </div>
+                </div>
+                <div className="card" style={{width: '35rem'}}>
+                    <img src={Employee} className="app-image card-img-top" alt="..." style={{height: '310px'}}/>
+                    <div class="card-body">
+                        <h5 class="card-title">Weather ForeCast Dashboard</h5>
+                        <a href="#" class="card-link">Github</a>
+                        <a href="#" class="card-link">App link</a>
+                    </div>
+                </div>
+            </div>
+            <div className='row' style={{display: 'flex', justifyContent: 'center'}}>
+            <div className="card" style={{width: '35rem'}}>
+                    <img src={Fishspots} className="app-image card-img-top" alt="..."/>
+                    <div class="card-body">
+                        <h5 class="card-title">Weather ForeCast Dashboard</h5>
+                        <a href="#" class="card-link">Github</a>
+                        <a href="#" class="card-link">App link</a>
+                    </div>
+                </div>
+                <div className="card" style={{width: '35rem'}}>
+                    <img src={Scheduler} className="app-image card-img-top" alt="..."/>
+                    <div class="card-body">
+                        <h5 class="card-title">Weather ForeCast Dashboard</h5>
+                        <a href="#" class="card-link">Github</a>
+                        <a href="#" class="card-link">App link</a>
+                    </div>
                 </div>
             </div>
         </div>
